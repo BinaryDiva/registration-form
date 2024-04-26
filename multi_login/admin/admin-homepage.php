739bd6f13bf22a7806517019d71b700081c5d80a
BinaryDiva <d00272896@student.dkit.ie>
2024-04-26 15:57:30 +0100

    changed togglebutton

--- multi_login/admin/admin-homepage.php
+++ multi_login/admin/admin-homepage.php
@@ -5,10 +5,38 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>MiTech Jobs</title>
     <link rel="stylesheet" href="../styles.css">
-    <script>
+   
+</head>
+<body>
+<nav class="navigation-bar">
+      <a href="#" class="logo">MiTech Inc.</a>
+      <ul class="main-menu">
+          <li class="menu-item"><a href="#">Home</a></li>
+          <li class="menu-item has-submenu"><a tabindex="0">Articles</a>
+            <ul class="submenu">
+                <li class="submenu-item"><a href="#">Technology</a></li>
+                <li class="submenu-item"><a href="#">Career</a></li>
+                <li class="submenu-item"><a href="#">Marketing</a></li>
+                <li class="submenu-item"><a href="#">Events</a></li>
+            </ul>
+          </li>
+          <li class="menu-item has-submenu"><a tabindex="0">Services</a>
+            <ul class="submenu">
+                <li class="submenu-item"><a href="#">UX and UI Design</a></li>
+                <li class="submenu-item"><a href="#">Web Development</a></li>
+                <li class="submenu-item"><a href="#">Digital Marketing</a></li>
+                <li class="submenu-item"><a href="#">Copywriting</a></li>
+            </ul>
+          </li>
+          <li class="menu-item"><a href="#">About Us</a></li>
+          <li class="menu-item"><a href="#">Contact Us</a></li>
+      </ul>
+      <span class="toggle-button" >&#9776;</span>
+  </nav>
+ <script>
         const toggleButton = document.querySelector(".toggle-button");
 const mainMenu = document.querySelector(".main-menu");
-
+console.log(toggleButton);
 toggleButton.addEventListener('click', function () {
     // Toggles the active class
     mainMenu.classList.toggle('active');
@@ -45,34 +73,6 @@ for (let menuItem of menuItems) {
 
 
     </script>
-</head>
-<body>
-<nav class="navigation-bar">
-      <a href="#" class="logo">MiTech Jobs.</a>
-      <ul class="main-menu">
-          <li class="menu-item"><a href="#">Home</a></li>
-          <li class="menu-item has-submenu"><a tabindex="0">Articles</a>
-            <ul class="submenu">
-                <li class="submenu-item"><a href="#">Technology</a></li>
-                <li class="submenu-item"><a href="#">Career</a></li>
-                <li class="submenu-item"><a href="#">Marketing</a></li>
-                <li class="submenu-item"><a href="#">Events</a></li>
-            </ul>
-          </li>
-          <li class="menu-item has-submenu"><a tabindex="0">Services</a>
-            <ul class="submenu">
-                <li class="submenu-item"><a href="#">UX and UI Design</a></li>
-                <li class="submenu-item"><a href="#">Web Development</a></li>
-                <li class="submenu-item"><a href="#">Digital Marketing</a></li>
-                <li class="submenu-item"><a href="#">Copywriting</a></li>
-            </ul>
-          </li>
-          <li class="menu-item"><a href="#">About Us</a></li>
-          <li class="menu-item"><a href="#">Contact Us</a></li>
-      </ul>
-      <span class="toggle-button" >&#9776;</span>
-  </nav>
-
 
 </body>
 </html>
